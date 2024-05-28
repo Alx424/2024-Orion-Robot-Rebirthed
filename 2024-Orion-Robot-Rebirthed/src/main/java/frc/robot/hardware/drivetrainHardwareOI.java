40350305f0d536c064cc2102b04983ae3c86d1d1
Alx424 <knowledgeablehamster@gmail.com>
2024-05-28 19:29:47 -0400

    Added drivetrain hardware interface file including default methods.

--- 2024-Orion-Robot-Rebirthed/src/main/java/frc/robot/hardware/drivetrainHardwareOI.java
+++ 2024-Orion-Robot-Rebirthed/src/main/java/frc/robot/hardware/drivetrainHardwareOI.java
@@ -1,5 +1,17 @@
 package frc.robot.hardware;
 
-public class drivetrainHardwareOI {
-    
+public interface drivetrainHardwareOI {
+    public void setLeftSidePower(double val);
+
+    public void setRightSidePower(double val);
+
+    public double getLeftEncoderPos();
+
+    public double getRightEncoderPos();
+
+    public void resetEncoderPos();
+
+    public void resetGyro();
+
+    public double getAngle();
 }
