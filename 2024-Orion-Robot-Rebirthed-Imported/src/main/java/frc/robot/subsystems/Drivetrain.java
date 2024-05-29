47a958f0461404eaf7d03e2ef53b4633e7ebe2ad
Alx424 <knowledgeablehamster@gmail.com>
2024-05-28 20:50:11 -0400

    ...

---
+++ 2024-Orion-Robot-Rebirthed-Imported/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,50 @@
+package frc.robot.subsystems;
+
+import frc.hardware.IDrivetrainHardware;
+import java.lang.Math;
+
+public class Drivetrain {
+    private IDrivetrainHardware hardware;
+    private double offsetAngle;
+
+    public Drivetrain(IDrivetrainHardware hardware) {
+        this.hardware = hardware;
+        offsetAngle = 0; // to be tuned
+    }
+
+    private double capInput(double val, double min, double max) {
+        if(val < min) return min;
+        if(val > max) return max;
+        return val;
+    }
+
+    public void arcadeDrive(double forward, double rotate) {
+        double leftSidePower = capInput(forward + rotate, -1., 1.);
+        double rightSidePower = capInput(forward - rotate, -1., 1.);
+        hardware.setLeftSidePower(leftSidePower);
+        hardware.setRightSidePower(rightSidePower);
+    }
+
+    public void resetEncoders() {
+        hardware.resetEncoders();
+    }
+
+    public double getEncoderPos() {
+        return ((hardware.getLeftEncoderPos() + hardware/getRightEncoderPos()) / 2);
+    }
+
+    public double getAngle() {
+        return hardware.getAngle() - offsetAngle;
+    }
+
+    public double getHeading() {
+        return hardware.getAngle % 360.;
+    }
+
+    private double convertEncoder(double encoderPos) {
+        // ratio needs to be tuned
+        double converterConst = -3.;
+        double convertedDist = encoderPos / converterConst;
+        return convertedDist;
+    }
+}
